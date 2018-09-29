76328e09146c946734f940602f32dd2724b485b0
ahob85 <aaron@ahob85.com>
2018-09-29 11:18:46 -0700

    update

---
+++ debugging.js
@@ -0,0 +1,38 @@
+// Remove block comments when ready to debug a program.
+
+// Program #1: "Careless"
+/*
+let x == 3;
+if(x => 3 {
+  X+++;
+}} Else {
+  X =+ 2;
+
+console.log = X;
+*/
+
+// Program #2: "Bad logic and math"
+/*
+let money = 28;
+if(money > 50 && money <= 20) {
+  money /= 2;
+} else {
+  money /= 0;
+}
+*/
+
+// Program #3: "And what else?"
+/*
+let num1 = READLINE.question("Enter num1: ");
+let num2 = READLINE.question("Enter num2: ");
+let num3 = READLINE.question("Enter num3: ");
+if(num1 > num2) {
+  console.log(`${num1} is the largest value.`);
+}
+else if(num2 > num3) {
+  console.log(`${num2} is the largest value.`);
+}
+else {
+  console.log(`${num3} is the largest value.`);
+}
+*/
