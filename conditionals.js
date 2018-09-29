9847c6e110d99e1ab7f3fc32a1d229c81b61111b
ahob85 <aaron@ahob85.com>
2018-09-28 18:47:47 -0700

    update

---
+++ conditionals.js
@@ -0,0 +1,60 @@
+let READLINE = require("readline-sync");
+
+// **** Problem 1: R-rated ****
+// You cannot see an R-rated movie unless you are at least 18, or you are with
+// an adult. Write code that prints whether or not someone can see an
+// R-rated movie.
+console.log("*** Problem 1: R-rated ***");
+let age = READLINE.question("Enter your age: ");
+let withAdult = READLINE.question("Are you with an adult? (yes or no): ");
+
+
+// **** Problem 2: Umbrella ****
+// You should bring an umbrella when you travel, but only if it is raining.
+// However, if it is thunderstorming, you should not bring an umbrella since
+// that's bad luck. Write code that prints if someone should bring an umbrella
+// with them.
+console.log("*** Problem 2: Umbrella ***");
+let raining = READLINE.question("Is it raining? (yes or no): ")
+
+
+// **** Problem 3: Monkey Trouble ****
+// There are two monkeys: Bubbles and Spankey. You are in trouble if both of
+// them are smiling, or if neither are smiling. Write code that prints if we
+// are in trouble.
+console.log("*** Problem 3: Monkey Trouble ***");
+let bubbles = READLINE.question("Is Bubbles smiling? (yes or no): ");
+let spankey = READLINE.question("Is Spankey smiling? (yes or no): ");
+
+
+// **** Problem 4: First Place ****
+// Write code that prints the largest of three scores. If there is a tie for
+// first place, simply print one of the tie winners.
+console.log("*** Problem 4: Largest Number ***");
+let score1 = READLINE.question("Enter the first score: ");
+let score2 = READLINE.question("Enter the second score: ");
+let score3 = READLINE.question("Enter the third score: ");
+
+
+// **** Problem 5: Phone Shopping ****
+// At a phone store, you can afford various "tiers" of phones based on how
+// much money you have.
+//    If you have at least $30 you can buy a prepaid phone.
+//    If you have at least $100 you can buy a bottom-tier phone.
+//    If you have at least $300 you can buy a middle-tier phone.
+//    If you have at least $600 you can buy a top-tier phone.
+// Write code that prints all the phone tiers someone can buy based on how much
+// money they have.
+console.log("*** Problem 5: Phone Shopping ***");
+let money = READLINE.question("How much money do you have?: ");
+
+
+// **** Problem 6: Guess My Number ****
+// Write code that plays a simple number guessing game with a user. You can
+// use any number you like as myNum. If the user guesses the number, print
+// a congratulations message. If they guess too low or too high, tell them this.
+// If their guess is myNum +/- 3, tell them they are "warm"; otherwise tell
+// them they are cold.
+console.log("*** Problem 6: Guess My Number ***");
+let myNum = 5;
+let guess = READLINE.question("I'm thinking of a number. Guess it: ");
--- haunted.js
+++ haunted.js
@@ -1,11 +1,16 @@
 // Author: FirstName LastName
-let readline = require("readline-sync");
+let READLINE = require("readline-sync");
 
 console.log();
-console.log("*****************************************************************");
-console.log("                          HAUNTED HOUSE");
-console.log("                 A game by FirstName LastName");
-console.log("*****************************************************************");
+console.log(` ('-. .-.   ('-.                      .-') _  .-') _     ('-.  _ .-') _         ('-. .-.                            .-')      ('-.
+( OO )  /  ( OO ).-.                 ( OO ) )(  OO) )  _(  OO)( (  OO) )       ( OO )  /                           ( OO ).  _(  OO)
+,--. ,--.  / . --. / ,--. ,--.   ,--./ ,--,' /     '._(,------.\     .'_       ,--. ,--. .-'),-----.  ,--. ,--.   (_)---\_)(,------.
+|  | |  |  | \-.  \  |  | |  |   |   \ |  |\ |'--...__)|  .---',`'--..._)      |  | |  |( OO'  .-.  ' |  | |  |   /    _ |  |  .---'
+|   .|  |.-'-'  |  | |  | | .-') |    \|  | )'--.  .--'|  |    |  |  \  '      |   .|  |/   |  | |  | |  | | .-') \  :` `.  |  |
+|       | \| |_.'  | |  |_|( OO )|  .     |/    |  |  (|  '--. |  |   ' |      |       |\_) |  |\|  | |  |_|( OO ) '..`''.)(|  '--.
+|  .-.  |  |  .-.  | |  | | `-' /|  |\    |     |  |   |  .--' |  |   / :      |  .-.  |  \ |  | |  | |  | | `-' /.-._)   \ |  .--'
+|  | |  |  |  | |  |('  '-'(_.-' |  | \   |     |  |   |  `---.|  '--'  /      |  | |  |   `'  '-'  '('  '-'(_.-' \       / |  `---.
+`--' `--'  `--' `--'  `-----'    `--'  `--'     `--'   `------'`-------'       `--' `--'     `-----'   `-----'     `-----'  `------' `);
 console.log("You are standing in front of a spooky haunted house.");
 console.log("You hear what sounds like screaming coming from inside the house.");
 let enterHouse = readline.question("Will you enter the house? (yes or no) ");
