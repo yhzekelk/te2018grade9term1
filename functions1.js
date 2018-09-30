389a1539233fc82265b9658aa6291dbc6b730264
ahob85 <aaron@ahob85.com>
2018-09-30 12:29:24 -0700

    update

--- functions1.js
+++ functions1.js
@@ -1,60 +1,33 @@
 const READLINE = require("readline-sync");
 
-
-let
-
-// **** Problem 1: Hello World ****
-// Write a function that, when called, prints "Hello world!" to the console.
-function helloWorld() {
-  console.log("Hello world!");
-}
-
-// **** Problem 2: What is your name? ****
-// You should bring an umbrella when you travel, but only if it is raining.
-// However, if it is thunderstorming, you should not bring an umbrella since
-// that's bad luck. Write code that prints if someone should bring an umbrella
-// with them.
-console.log("*** Problem 2: Umbrella ***");
-let raining = READLINE.question("Is it raining? (yes or no): ")
+// **** Problem 1: printMessage() ****
+// Write a function that prints any message you want.
 
 
-// **** Problem 3: Monkey Trouble ****
-// There are two monkeys: Bubbles and Spankey. You are in trouble if both of
-// them are smiling, or if neither are smiling. Write code that prints if we
-// are in trouble.
-console.log("*** Problem 3: Monkey Trouble ***");
-let bubbles = READLINE.question("Is Bubbles smiling? (yes or no): ");
-let spankey = READLINE.question("Is Spankey smiling? (yes or no): ");
+// **** Problem 2: printFiveMessages() ****
+// Write a function that calls printMessage() five times.
 
 
-// **** Problem 4: First Place ****
-// Write code that prints the largest of three scores. If there is a tie for
-// first place, simply print one of the tie winners.
-console.log("*** Problem 4: First Place ***");
-let score1 = READLINE.question("Enter the first score: ");
-let score2 = READLINE.question("Enter the second score: ");
-let score3 = READLINE.question("Enter the third score: ");
+// **** Problem 3: getUserInput() ****
+// Write a function that asks the user if they'd like to print your message
+// once or five times. Then call one of the two functions above based on
+// what the user decides.
 
 
-// **** Problem 5: Phone Shopping ****
-// At a phone store, you can afford various "tiers" of phones based on how
-// much money you have.
-//    If you have at least $30 you can buy a prepaid phone.
-//    If you have at least $100 you can buy a bottom-tier phone.
-//    If you have at least $300 you can buy a middle-tier phone.
-//    If you have at least $600 you can buy a top-tier phone.
-// Write code that prints all the phone tiers someone can buy based on how much
-// money they have.
-console.log("*** Problem 5: Phone Shopping ***");
-let money = READLINE.question("How much money do you have?: ");
+// **** Problem 4: printGreeting() ****
+// Write a function that prints a greeting message to the user.
 
 
-// **** Problem 6: Guess My Number ****
-// Write code that plays a simple number guessing game with a user. You can
-// use any number you like as myNum. If the user guesses the number, print
-// a congratulations message. If they guess too low or too high, tell them this.
-// If their guess is myNum +/- 3, tell them they are "warm"; otherwise tell
-// them they are cold.
-console.log("*** Problem 6: Guess My Number ***");
-let myNum = 5;
-let guess = READLINE.question("I'm thinking of a number. Guess it: ");
+// **** Problem 5: printClosing() ****
+// Write a function that prints a goodbye message to the user.
+
+
+// **** Problem 6: run() ****
+// Write a function that runs the whole program by calling functions in this
+// order:
+// 1) printGreeting()
+// 2) getUserInput()
+// 3) printClosing()
+
+
+// When finished, call your run() function below to run the whole program!
