99bad1e8e33ce5557977c128d6170bf256a849ea
Yhzekel <yhzekelk@caedge.org>
2018-10-06 13:18:56 -0700

    I created several variables. And showed a "Hello Message" world.

--- data.js
+++ data.js
@@ -3,3 +3,15 @@ let message = "Hello World!";
 console.log(message);
 
 // Type your code below!
+var birthday = true;
+var alive = true;
+var age = 14;
+var year = 2018;
+var quantity = 1;
+
+//var
+var myBirthday = 'birthday';
+var populationQuantity = 'quantity'
+
+var lifeform = undefined;
+var name = undefined;
